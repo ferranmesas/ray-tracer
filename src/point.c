541e0cebaf4b02ed9019f1d452d301456517959d
ferran <ferran.mesas01>
2016-08-19 20:38:48 +0200

    Implement distance between two points
    Name conflicts with method in file sphere

--- src/point.c
+++ src/point.c
@@ -1,4 +1,13 @@
 #include "point.h"
+#include "math.h"
+
+float distance(point *p, point *q) {
+    return sqrt(
+      (p->x - q->x) * (p->x - q->x) +
+      (p->y - q->y) * (p->y - q->y) +
+      (p->z - q->z) * (p->z - q->z)
+    );
+}
 
 void print_point (point *p){
  fprintf(stderr, "(%f, %f, %f)\n", p->x, p->y, p->z);
--- src/point.h
+++ src/point.h
@@ -6,6 +6,8 @@ typedef struct {
   float x, y, z;
 } point;
 
+float distance(point *p, point *q);
+
 void print_point(point *p);
 
 #endif // POINT_H
