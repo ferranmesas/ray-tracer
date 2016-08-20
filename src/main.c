42f393fb7e9ab55ac388060ec94cc1a8bcdfa875
ferran <ferran.mesas01>
2016-08-21 01:22:13 +0200

    implement basic ray-marching
    todo:
    picture plane, proper camera and camera direction
    anti-aliasing (multiple rays per pixel)

--- src/main.c
+++ src/main.c
@@ -1,13 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "image.h"
 #include "sphere.h"
 #include "point.h"
 #include "ray.h"
 
+#define EPS 1.0E-3
+#define MAX_ITER 100
+
+int intersects(ray r, sphere *spheres, int n_spheres, ray *normal);
+
 int main(int argc, char* argv[]) {
-  long height, width;
+  long height, width, min_dim;
   if(argc == 3) {
     width = strtol(argv[1], NULL, 10);
     height = strtol(argv[2], NULL, 10);
@@ -17,15 +23,15 @@ int main(int argc, char* argv[]) {
     height = 100;
   }
 
+  min_dim = height < width ? height : width;
+
   image *im = (image*) malloc(sizeof(image));
   init_image(im, width, height);
-  point *camera = (point*) malloc(sizeof(point));
-  point *dir = (point*) malloc(sizeof(point));
-  point * light = malloc(sizeof(point));
 
-  scanf("%f %f %f\n", &(camera->x), &(camera->y), &(camera->z));
-  scanf("%f %f %f\n", &(dir->x), &(dir->y), &(dir->z));
-  scanf("%f %f %f\n", &(light->x), &(light->y), &(light->z));
+  point camera, dir, light_source;
+  scanf("%f %f %f\n", &(camera.x), &(camera.y), &(camera.z));
+  scanf("%f %f %f\n", &(dir.x), &(dir.y), &(dir.z));
+  scanf("%f %f %f\n", &(light_source.x), &(light_source.y), &(light_source.z));
 
   int n_spheres;
   scanf("%d\n", &n_spheres);
@@ -36,25 +42,59 @@ int main(int argc, char* argv[]) {
     scanf("%f %f %f %f\n", &(s->x), &(s->y), &(s->z), &(s->r));
   }
 
-  for(int i = 0; i < im->height; i++) {
-    float u = 2.0f * i / (height-1) - 1;
-    for(int j = 0; j < im->width; j++) {
-      float v = 2.0f * j / (width - 1) - 1;
-      ray r ={
-        .source = {u, 2, v},
-        .direction = *dir
-      };
+  // Done reading input, start tracing!
 
-      for (int k = 0; k < n_spheres; k++) {
-        float dist = distance(&(r.source), &spheres[k].center);
-        char color = dist / spheres[k].r * 255;
-        if (distance(&(r.source), &spheres[k].center) < spheres[k].r) {
-          im->data[i * im->width + j] = color;
-        }
+  for (int i = 0; i < im->height; i++) {
+    for (int j = 0; j < im->width; j++) {
+      // both u and v are scaled by the same value (min_dim) in order to preserve
+      // the aspect ratio.
+      float u = 2.0f * i / (min_dim - 1) - (1.0f * height / min_dim);
+      float v = 2.0f * j / (min_dim - 1) - (1.0f * width / min_dim);
+
+      // TODO: Calculate proper ray source and direction for each pixel!
+      // need to adjust for perspective
+      ray current_ray = {
+        .source = camera,
+        .dir = {u, 10, v}
+      };
+      normalize(&current_ray.dir);
+
+      ray normal;
+
+      if (intersects(current_ray, spheres, n_spheres, &normal)) {
+        ray light;
+        ray_from_to(&light, light_source, normal.source);
+
+        float light_incidence = dot_product(normal.dir, light.dir);
+        light_incidence = light_incidence > 0? light_incidence : 0;
+        im->data[i * im->width + j] = 32 + 224 * light_incidence;
       }
     }
   }
 
   print_image(im);
   destroy_image(im);
+}
+
+int intersects(ray r, sphere *spheres, int n_spheres, ray *normal) {
+  for (int i = 0; i < MAX_ITER; i++){
+    // find closest sphere and distance to it.
+    float min_dist = INFINITY;
+    for (int k = 0; k < n_spheres; k++) {
+      float dist = sphere_distance(spheres[k], r.source);
+      if (dist < min_dist) {
+        min_dist = dist;
+        ray_from_to(normal, spheres[k].center, r.source);
+        ray_reverse(normal);
+      }
+    }
+    // advance the minimum distance.
+    ray_advance(&r, min_dist);
+
+    // we got real close to an object (there was an intersection)
+    if (min_dist < EPS) {
+      return 1;
+    }
+  }
+  return 0;
 }
