05d5ec63898b2a2b5db0f47abf7e146148f64214
eyeos <eyeos@localhost.localdomain>
2016-08-22 16:03:47 +0200

    add shadows!
    i don't like how shadows work right now, need to think of a better way

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 #define EPS 1.0E-3
 #define MAX_ITER 2048
 #define MAX_DIST 100.0f
-#define N_RAYS 4
+#define N_RAYS 16
 
 int intersects(ray r, const scene s, point *intersection);
 
@@ -71,8 +71,9 @@ int main(int argc, char* argv[]) {
         };
         normalize(&current_ray.dir);
 
-        point intersection;
-        ray normal, light, reflection, light_reflection;
+        point intersection, light_intersection;
+        ray normal, light;
+        ray reflection, light_reflection;
         if (intersects(current_ray, s, &intersection)) {
           scene_get_normal(s, intersection, &normal);
           float fog_amount = distance(camera, normal.source) / MAX_DIST;
@@ -81,10 +82,20 @@ int main(int argc, char* argv[]) {
           ray_reflect(current_ray, normal, &reflection);
           ray_reflect(light, normal, &light_reflection);
 
-          float diffuse_light = max(0, dot_product(normal.dir, light.dir));
-          float specular_light = max(0, -dot_product(light_reflection.dir, current_ray.dir));
-          float light = 0.1 + 0.6 * diffuse_light + 0.3 * pow(specular_light, 10);
-          total_light += fog_amount * 0.8 + (1 - fog_amount) * light;
+
+
+
+          float diffuse_light = dot_product(normal.dir, light.dir);
+
+          if (diffuse_light > 0) {
+            intersects(light, s, &light_intersection);
+            if(distance(light_intersection, intersection) < EPS * 40) {
+
+              float specular_light = max(0, -dot_product(light_reflection.dir, current_ray.dir));
+              float light = 0.1 + 0.6 * diffuse_light + 0.3 * pow(specular_light, 10);
+              total_light += fog_amount * 0.8 + (1 - fog_amount) * light;
+            }
+          }
         }
       }
       putchar(50 * total_light / N_RAYS);
